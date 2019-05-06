c4120fbe718e5769dce66ebab63b271a5d89c5dc
blattersturm <peachypies@protonmail.ch>
2019-05-06 13:37:27 +0200

    Add spawn audio advertising the server project name.

---
+++ resources/[expeditious]/execution-audio/__resource.lua
@@ -0,0 +1,9 @@
+client_script 'spawn_sound.lua'
+
+files {
+	'data/dlcexpeditious_sounds.dat54.rel',
+	'dlc_expeditious/expeditious.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'dlc_expeditious'
+data_file 'AUDIO_SOUNDDATA' 'data/dlcexpeditious_sounds.dat'
---
+++ resources/[expeditious]/execution-audio/spawn_sound.lua
@@ -0,0 +1,5 @@
+AddEventHandler('playerSpawned', function()
+    PlaySoundFrontend(-1, 'Expeditious_Spawn', 'DLC_EXPEDITIOUS_SOUNDS', 1)
+end)
+
+RequestScriptAudioBank('dlc_expeditious/expeditious', 0)
